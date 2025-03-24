a7ec570653a49d2e3ad4037784d5b71453af18fd
晏迪(Yan Di) <82530115+1991785628@users.noreply.github.com>
2025-03-24 22:00:17 +0800

    Update server.hpp

--- datablocker/head/server.hpp
+++ datablocker/head/server.hpp
@@ -1,27 +1,20 @@
-#include "server.hpp"
-#include "session.hpp"
-#include <iostream>
+#ifndef SERVER_HPP
+#define SERVER_HPP
 
-const size_t MAX_CONNECTION = 100;  //可自定义最大连接数量
+#include <boost/asio.hpp>
+#include <atomic>
 
-Server::Server(boost::asio::io_context& io_context, short port)
-    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
-    accept();
-}
+using boost::asio::ip::tcp;
 
-void Server::accept() {
-    acceptor_.async_accept(
-        [this](boost::system::error_code ec, tcp::socket socket) {
-            if (!ec) {
-                if (current_connections_ < MAX_CONNECTION) {
-                    std::make_shared<Session>(std::move(socket))->start();
-                    current_connections_++;
-                    std::cout << "New connection accepted. Total: " 
-                              << current_connections_ << "\n";
-                } else {
-                    std::cout << "Connection refused: maximum limit reached\n";
-                }
-            }
-            accept();
-        });
-}
+class Server {
+public:
+    Server(boost::asio::io_context& io_context, short port);
+
+private:
+    void accept();
+    
+    tcp::acceptor acceptor_;
+    std::atomic<int> current_connections_{0};
+};
+
+#endif
